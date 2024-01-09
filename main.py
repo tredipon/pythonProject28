f385febefac91df5f67c644b791ca6e5e6f90180
Sasha <tredipon@gmail.com>
2024-01-09 17:06:12 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logging.info("Інформація про рядок коду")
+logging.debug("")
+logging.warning("Попередження про можливу помилку")
+logging.error("Помилка в програмі")
+logging.critical("Критична помилка")
+
+
